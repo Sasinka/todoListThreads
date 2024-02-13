ac3de65a83a2d5299ece60356a09c3934b834697
Sára R <roseckasara@gmail.com>
2024-02-13 16:43:08 +0100

    Initial commit

---
+++ src/main/java/com/example/todolistthreads/TodoListThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListThreadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todolistthreads/TodoListThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
